13847cdbe06f89e538933c391c33928308dacffb
Yan Klyuzhin <klyuzhin@uchi.ru>
2023-10-03 10:02:34 +0300

    styled payment

--- src/features/Payment/ui/Payment/Payment.tsx
+++ src/features/Payment/ui/Payment/Payment.tsx
@@ -1,8 +1,9 @@
-import { useContext, Fragment } from 'react';
-import { Loader } from 'shared/ui';
+import { useContext } from 'react';
+import { Button, Loader } from 'shared/ui';
 import { ActionContext, GetStateContext, PayState } from 'store';
 import { Stages, TransitionTypes } from 'store/enums';
 import { usePay } from '../../model';
+import classes from './Payment.module.scss';
 
 export const Payment = () => {
     const state = useContext(GetStateContext)();
@@ -34,33 +35,31 @@ export const Payment = () => {
 
     if (stage === Stages.PAID)
         return (
-            <>
-                Thank you for your purchase!
-                <button type="button" onClick={onNewPurchaseClick}>
-                    New purchase
-                </button>
-            </>
+            <div className={classes.thankYou}>
+                <h1>Thank you for your purchase!</h1>
+                <Button onClick={onNewPurchaseClick} text="New purchase" />
+            </div>
         );
 
     return (
-        <div>
-            <br />
+        <div className={classes.payment}>
             {productsInCart.map(({ id, title, price, inCartAmount }, index) => (
-                <Fragment key={id}>
-                    {index + 1}. {title} | {inCartAmount}*${price}
-                    <br />
-                </Fragment>
+                <div key={id} className={classes.product}>
+                    <span>
+                        {index + 1}. {title}
+                    </span>
+                    <span>
+                        {inCartAmount}*${price}
+                    </span>
+                </div>
             ))}
-            <br />
-            Total price: ${totalPrice}
-            <br />
-            {stage === Stages.PAYING ? (
-                <Loader />
-            ) : (
-                <button type="button" onClick={onPayClick}>
-                    Pay
-                </button>
-            )}
+            <div className={classes.pay}>
+                <div className={classes.price}>Total price: ${totalPrice}</div>
+                {stage === Stages.PAYMENT && (
+                    <Button onClick={onPayClick} text="Pay" />
+                )}
+            </div>
+            {stage === Stages.PAYING && <Loader />}
         </div>
     );
 };
